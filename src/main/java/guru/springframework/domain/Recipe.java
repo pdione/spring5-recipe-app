c3f3e225564300842616ab0a197d839a22080f26
pdione <dione.pape@gmail.com>
2017-11-05 20:38:51 +0100

    Adding Recipe - Ingredient @OnetoMany relationship

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,11 +1,14 @@
 package guru.springframework.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -29,6 +32,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
 	return id;
@@ -108,6 +114,14 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
 	this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+	return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+	this.ingredients = ingredients;
     }
 
 }
