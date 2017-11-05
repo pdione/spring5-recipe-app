f016f5532836a953510fbbde1f08681040402084
pdione <dione.pape@gmail.com>
2017-11-05 20:52:56 +0100

    Adding Ingredient UnitOfMeasure @OneToOne relationship

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -3,10 +3,12 @@ package guru.springframework.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -17,10 +19,12 @@ public class Ingredient {
 
     private String description;
     private BigDecimal amount;
-    // private UnitOfMesure uom;
 
     @ManyToOne
     private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public String getDescription() {
 	return description;
@@ -44,6 +48,14 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
 	this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+	return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+	this.uom = uom;
     }
 
 }
