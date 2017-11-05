36ba085e217c91fbf8b5db561e0510d190d03250
pdione <dione.pape@gmail.com>
2017-11-05 20:58:35 +0100

    Adding difficulty Enum

---
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package guru.springframework.domain;
+
+public enum Difficulty {
+    EASY, MOODERATE, KINF_OF_HARD, HARD;
+}
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -29,6 +31,9 @@ public class Recipe {
     // private Difficulty difficulty;
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -122,6 +127,14 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
 	this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+	return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+	this.difficulty = difficulty;
     }
 
 }
