28178e61cb7635f98c2f1fff8af5b95adb8e075f
pdione <dione.pape@gmail.com>
2017-11-07 23:04:48 +0100

    Introducing query methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,14 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 
 /**
  * Index Controller.
@@ -12,9 +19,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+	super();
+	this.categoryRepository = categoryRepository;
+	this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({ "", "/", "index" })
     public String getIndexPage() {
-	System.out.println("Something to say...");
+
+	Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+	Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+	System.out.println("Cat ID is:" + categoryOptional.get().getId());
+	System.out.println("UOM ID is:" + unitOfMeasureOptional.get().getId());
+
 	return "index";
     }
 
--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,5 +1,7 @@
 package guru.springframework.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import guru.springframework.domain.Category;
@@ -10,5 +12,7 @@ import guru.springframework.domain.Category;
  *
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 
 }
