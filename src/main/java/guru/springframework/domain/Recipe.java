eea6cab091cf3cb9c22a862f9587185486e90467
pdione <dione.pape@gmail.com>
2017-11-07 22:10:25 +0100

    Adding Recipe Category @ManyToMany relationship

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -9,7 +9,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -27,8 +30,7 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO add
-    // private Difficulty difficulty;
+
     @Lob
     private Byte[] image;
 
@@ -40,6 +42,10 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
 	return id;
@@ -135,6 +141,14 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
 	this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+	return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+	this.categories = categories;
     }
 
 }
