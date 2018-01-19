d16bc862b80d71cda2328ddd2dacf1f8830224b8
pdione <dione.pape@gmail.com>
2018-01-19 15:57:29 +0100

    Displaying images from database.

--- src/main/java/guru/springframework/converters/RecipeToRecipeCommand.java
+++ src/main/java/guru/springframework/converters/RecipeToRecipeCommand.java
@@ -9,7 +9,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by jt on 6/21/17.
+ * Created by jt boumi.
  */
 @Component
 public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand>{
@@ -43,6 +43,7 @@ public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand>{
         command.setServings(source.getServings());
         command.setSource(source.getSource());
         command.setUrl(source.getUrl());
+        command.setImage(source.getImage());
         command.setNotes(notesConverter.convert(source.getNotes()));
 
         if (source.getCategories() != null && source.getCategories().size() > 0){
